4fe2093f346bf1b2c1a5f48f6deb8944f9e0ba20
v-king777 <v.kolpashchikov@gmail.com>
2021-05-21 23:57:14 +0300

    Добавьте файлы проекта.

---
+++ Task/Contact.cs
@@ -0,0 +1,21 @@
+﻿namespace Task
+{
+    internal class Contact
+    {
+        public Contact(string name, string lastName, long phoneNumber, string email)
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public string Name { get; }
+
+        public string LastName { get; }
+
+        public long PhoneNumber { get; }
+
+        public string Email { get; }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var phoneBook = new List<Contact>();
+
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            // LINQ
+            var sortedPhoneBook1 = from c in phoneBook
+                                   orderby c.Name, c.LastName
+                                   select c;
+
+            // Методы расширения
+            var sortedPhoneBook2 = phoneBook
+                .OrderBy(c => c.Name)
+                .ThenBy(c => c.LastName);
+
+            // Вывод результата
+            foreach (var contact in sortedPhoneBook1)
+            {
+                Console.WriteLine($"{contact.Name} - {contact.LastName}" +
+                    $" - {contact.PhoneNumber} - {contact.Email}");
+            }
+        }
+    }
+}
